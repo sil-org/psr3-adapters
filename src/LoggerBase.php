bcf4ff2234af6ddd0031e9666a7fea65ed6a0b5c
Matt Henderson <matt_henderson@sil.org>
2017-05-09 15:09:06 -0400

    Enable logging arrays of data.

--- src/LoggerBase.php
+++ src/LoggerBase.php
@@ -8,17 +8,47 @@ namespace Sil\Psr3Adapters;
 abstract class LoggerBase extends \Psr\Log\AbstractLogger
 {
     /**
+     * @var bool Whether the system this adapter connects to can support an
+     *     array as the log message (for more structured logs).
+     */
+    protected $isArraySupported = false;
+    
+    /**
      * Interpolate context values into the message placeholders.
+     * 
+     * @param string|array $message The data to be logged.
+     * @param array $context (Optional:) The array of values to insert into the
+     *     corresponding placeholders.
+     * @return mixed The resulting log message.
+     */
+    protected function interpolate($message, array $context = [])
+    {
+        if (is_string($message)) {
+            return $this->interpolateString($message, $context);
+        }
+        
+        if (is_array($message) && $this->isArraySupported) {
+            return \array_merge($message, $context);
+        }
+        
+        return $this->interpolateString(
+            var_export($message, true),
+            $context
+        );
+    }
+    
+    /**
+     * Interpolate context values into the given string.
      * 
      * This is based heavily on the example implementation here:
      * https://github.com/php-fig/fig-standards/blob/master/accepted/PSR-3-logger-interface.md#12-message
      * 
      * @param string $message The message (potentially with placeholders).
-     * @param array $context The array of values to insert into the
+     * @param array $context (Optional:) The array of values to insert into the
      *     corresponding placeholders.
      * @return string The resulting string.
      */
-    protected function interpolate($message, array $context = [])
+    private function interpolateString($message, array $context = [])
     {
         // Build a replacement array with braces around the context keys.
         $replace = [];
