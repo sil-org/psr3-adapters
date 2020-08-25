7117db16edcd6465cd2ad77c10fad57cd8be436d
Mark Tompsett <mtompset@hotmail.com>
2020-08-25 13:04:27 -0400

    Add Fake Logger Tests

--- tests/Psr3FakeLoggerTest.php
+++ tests/Psr3FakeLoggerTest.php
@@ -1,21 +1,32 @@
 <?php
 namespace Sil\Psr3Adapters\tests;
 
+use PHPUnit\Framework\Assert;
 use PHPUnit\Framework\TestCase;
-use Sil\Psr3Adapters\Psr3SamlLogger;
+use Sil\Psr3Adapters\Psr3FakeLogger;
 
-class Psr3SamlLoggerTest extends TestCase
+class Psr3FakeLoggerTest extends TestCase
 {
-    public function testLogUnknownLogLevel()
+    public function testHasLogs()
     {
-        // Arrange:
-        $psr3SamlLogger = new Psr3SamlLogger();
-        $psrLevel = 'unknown'; // Some unknown log level.
-        
-        // Pre-assert:
-        $this->expectException('\Psr\Log\InvalidArgumentException');
-        
-        // Act:
-        $psr3SamlLogger->log($psrLevel, 'Some message');
+        $psr3FakeLogger = new Psr3FakeLogger();
+        $psr3FakeLogger->log('warning', 'Some message');
+        $hasLogs = $psr3FakeLogger->hasLogs();
+        Assert::assertTrue($hasLogs, 'Missing expected log entry');
+    }
+
+    public function testHasSpecificLog()
+    {
+        $psr3FakeLogger = new Psr3FakeLogger();
+        $psr3FakeLogger->log('warning', 'Specific message');
+
+        $hasSpecificLog = $psr3FakeLogger->hasSpecificLog('Specific', false);
+        Assert::assertTrue($hasSpecificLog, 'Missing expected log entry (loose)');
+
+        $hasSpecificLog = $psr3FakeLogger->hasSpecificLog('Specific', true);
+        Assert::assertFalse($hasSpecificLog, 'Missing expected log entry (tight)');
+
+        $hasSpecificLog = $psr3FakeLogger->hasSpecificLog('LOG: [warning] Specific message', true);
+        Assert::assertTrue($hasSpecificLog, 'Missing expected specific log entry (tight)');
     }
 }
