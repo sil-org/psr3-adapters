a6d22e87c29b6cbce455e69799371147f97f4fa5
Mark Tompsett <mtompset@hotmail.com>
2021-03-24 17:28:50 -0400

    Address comments in PR
    - Removed isArraySupported, since only Psr3Yii2Logger supports arrays
    - Removed interpolateArray() call in favour of an inline array_merge()
    - Strongly type interpolateString()

--- src/LoggerBase.php
+++ src/LoggerBase.php
@@ -10,12 +10,6 @@ use Psr\Log\AbstractLogger;
 abstract class LoggerBase extends AbstractLogger
 {
     /**
-     * @var bool Whether the system this adapter connects to can support an
-     *     array as the log message (for more structured logs).
-     */
-    protected $isArraySupported = false;
-    
-    /**
      * Interpolate context values into the message placeholders.
      * 
      * @param string $message The data to be logged.
@@ -36,19 +30,6 @@ abstract class LoggerBase extends AbstractLogger
     }
 
     /**
-     * Interpolate context values into the message placeholders.
-     *
-     * @param array $message The data to be logged.
-     * @param array $context (Optional:) The array of values to insert into the
-     *     corresponding placeholders.
-     * @return array The resulting log message.
-     */
-    protected function interpolateArray($message, array $context = []): array
-    {
-        return \array_merge($message, $context);
-    }
-
-    /**
      * Interpolate context values into the given string.
      * 
      * This is based heavily on the example implementation here:
@@ -59,7 +40,7 @@ abstract class LoggerBase extends AbstractLogger
      *     corresponding placeholders.
      * @return string The resulting string.
      */
-    private function interpolateString($message, array $context = [])
+    private function interpolateString(string $message, array $context = []): string
     {
         // Build a replacement array with braces around the context keys.
         $replace = [];
